7165e8a99ea9182efad899f598ca11acaac7510d
Do Xuan Long <longdo@dobtech.vn>
2022-11-19 02:41:29 +0700

    added code to verify contract programatically

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,32 @@
-const { ethers } = require('hardhat');
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
-  const simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
-  console.log('Deploying contract...');
+  const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...");
   const simpleStorage = await simpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to: ${simpleStorage.address}`);
+  // only run verify if deployed to chain on etherscan and have api key
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 main()
