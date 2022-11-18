458f78d4f058ae32124b181b7af61aff5239125a
Do Xuan Long <longdo@dobtech.vn>
2022-11-19 02:13:56 +0700

    added goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require('@nomicfoundation/hardhat-toolbox');
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.7',
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  solidity: "0.8.7",
 };
