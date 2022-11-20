04983383fb404ff8fa254e0c0fa06950fbd29dea
Do Xuan Long <longdo@dobtech.vn>
2022-11-20 14:35:42 +0700

    added test for initial favorite number value

---
+++ test/test-deploy.js
@@ -0,0 +1,17 @@
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SimpleStorage", function () {
+  let simpleStorageFactory, simpleStorage;
+
+  beforeEach(async function () {
+    simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    simpleStorage = await simpleStorageFactory.deploy();
+  });
+
+  it("Should start with a favorite number of 0", async function () {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedValue = "0";
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+});
